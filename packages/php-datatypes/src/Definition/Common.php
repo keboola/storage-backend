e641e319fb0e472acde8d6f008a3d4fe32ed4ee5
pivnicek <marc@keboola.com>
2017-04-18 12:53:01 +0200

    feat: format, default, toMetadata

--- packages/php-datatypes/src/Definition/Common.php
+++ packages/php-datatypes/src/Definition/Common.php
@@ -16,6 +16,14 @@ class Common
      * @var bool
      */
     protected $nullable = false;
+    /**
+     * @var string
+     */
+    protected $default;
+    /**
+     * @var string
+     */
+    protected $format;
 
     /**
      * Common constructor.
@@ -24,11 +32,18 @@ class Common
      * @param string $length
      * @param bool $nullable
      */
-    public function __construct($type, $length = null, $nullable = true)
+
+    public function __construct($type, $length = null, $nullable = true, $default = null, $format = null)
     {
         $this->type = $type;
         $this->length = $length;
         $this->nullable = (bool) $nullable;
+        if ($nullable and is_null($default)) {
+            $this->default = "NULL";
+        } else {
+            $this->default = $default;
+        }
+        $this->format = $format;
     }
 
     /**
@@ -53,6 +68,22 @@ class Common
     public function isNullable()
     {
         return $this->nullable;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefault()
+    {
+        return $this->default;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
     }
 
     /**
@@ -106,5 +137,44 @@ class Common
             "length" => $this->getLength(),
             "nullable" => $this->isNullable()
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function toMetadata()
+    {
+        $metadata = [
+            [
+                "key" => "KBC.datatype.type",
+                "value" => $this->getType(),
+            ],[
+                "key" => "KBC.datatype.nullable",
+                "value" => $this->nullable
+            ],[
+                "key" => "KBC.datatype.basetype",
+                "value" => $this->getBasetype()
+            ]
+        ];
+        if ($this->length) {
+            $metadata[] = [
+                "key" => "KBC.datatype.length",
+                "value" => $this->length
+            ];
+        }
+        if ($this->default) {
+            $metadata[] = [
+                "key" => "KBC.datatype.default",
+                "value" => $this->default
+            ];
+        }
+        if ($this->format) {
+            $metadata[] = [
+                "key" => "KBC.datatype.format",
+                "value" => $this->format
+            ];
+        }
+
+        return $metadata;
     }
 }
