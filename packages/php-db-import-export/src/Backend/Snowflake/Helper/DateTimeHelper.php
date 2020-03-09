4bb11419a104b0c87dab9e569284cebca4024bd7
Martin Zajic <zajca@zajca.cz>
2020-03-09 13:16:26 +0100

    use const in datetime helper

--- packages/php-db-import-export/src/Backend/Snowflake/Helper/DateTimeHelper.php
+++ packages/php-db-import-export/src/Backend/Snowflake/Helper/DateTimeHelper.php
@@ -6,9 +6,11 @@ namespace Keboola\Db\ImportExport\Backend\Snowflake\Helper;
 
 final class DateTimeHelper
 {
+    public const FORMAT = 'Y-m-d H:i:s';
+
     public static function getNowFormatted(): string
     {
         $currentDate = new \DateTime('now', new \DateTimeZone('UTC'));
-        return $currentDate->format('Y-m-d H:i:s');
+        return $currentDate->format(self::FORMAT);
     }
 }
