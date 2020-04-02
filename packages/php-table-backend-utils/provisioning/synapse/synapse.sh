9d0fa648bdc3b617fc360af2311a529ad920222d
Martin Zajic <zajca@zajca.cz>
2020-04-02 20:23:19 +0200

    show created, use random deployment name

--- packages/php-table-backend-utils/provisioning/synapse/synapse.sh
+++ packages/php-table-backend-utils/provisioning/synapse/synapse.sh
@@ -3,6 +3,7 @@
 set -o pipefail         # Use last non-zero exit code in a pipeline
 
 #REQUIRED ENV VARS
+#SYNAPSE_SERVER_NAME=
 #AZURE_RESOURCE_GROUP=
 #AZURE_SERVICE_PRINCIPAL_TENANT=
 #AZURE_SERVICE_PRINCIPAL=
@@ -27,9 +28,10 @@ runCliCmd(){
 
 createServer(){
     export SYNAPSE_SERVER_PASSWORD=`openssl rand -base64 32`
+    export DEPLOYMENT_NAME=${SYNAPSE_SERVER_NAME}"_"`openssl rand -hex 5`
 
     local output=$(runCliCmd "group deployment create \
-  --name ${SYNAPSE_SERVER_NAME} \
+  --name ${DEPLOYMENT_NAME} \
   --resource-group ${AZURE_RESOURCE_GROUP} \
   --template-file /keboola/provisioning/synapse/synapse.json \
   --output json \
@@ -43,24 +45,24 @@ createServer(){
     export SYNAPSE_RESOURCE_ID=$(echo ${output} | jq -r '.properties.outputs.warehouseResourceId.value')
 
     echo "Server deployed."
-    echo $output
+    echo $SYNAPSE_SQL_SERVER_NAME
+    echo $SYNAPSE_DW_SERVER_NAME
+    echo $SYNAPSE_RESOURCE_ID
 
     local output=$(runCliCmd "sql server firewall-rule create \
   --resource-group ${AZURE_RESOURCE_GROUP} \
   --server ${SYNAPSE_SQL_SERVER_NAME} \
-  -n all \
+  --name all \
   --start-ip-address 0.0.0.0 \
   --end-ip-address 255.255.255.255")
 
     echo "Firewall rule set."
-    echo $output
 
 #    Set vars for php app
     export SYNAPSE_UID=keboola
     export SYNAPSE_PWD=${SYNAPSE_SERVER_PASSWORD}
     export SYNAPSE_DATABASE=${SYNAPSE_DW_SERVER_NAME}
     export SYNAPSE_SERVER=${SYNAPSE_DW_SERVER_NAME}.database.windows.net
-    exit 0
 }
 
 deleteServer(){
@@ -74,12 +76,10 @@ deleteServer(){
 
       local output=$(runCliCmd "sql server delete -y \
   --resource-group ${AZURE_RESOURCE_GROUP} \
-  --server ${SYNAPSE_SQL_SERVER_NAME}")
+  --name ${SYNAPSE_SQL_SERVER_NAME}")
 
   echo "Logical SQL server deleted."
   echo $output
-
-  exit 0
 }
 
 
@@ -112,7 +112,7 @@ while [[ $# -gt 0 ]]; do
             deleteServer
             ;;
         *)
-            script_exit "Invalid parameter was provided: $param" 1
+            script_usage
             ;;
     esac
 done
