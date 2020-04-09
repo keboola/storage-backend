4955a2f419a02bf27eecf2ed8ae355158b10ccef
Martin Zajic <zajca@zajca.cz>
2020-04-09 15:52:05 +0200

    fix source def

--- packages/php-db-import-export/tests/functional/Snowflake/OtherImportTest.php
+++ packages/php-db-import-export/tests/functional/Snowflake/OtherImportTest.php
@@ -233,12 +233,16 @@ class OtherImportTest extends SnowflakeImportExportBaseTest
             false,
             ImportOptions::SKIP_FIRST_LINE
         );
-        $source = new Storage\Snowflake\Table(self::SNOWFLAKE_SOURCE_SCHEMA_NAME, 'nullify_src');
-        $destination = new Storage\Snowflake\Table(self::SNOWFLAKE_DEST_SCHEMA_NAME, 'nullify', [
-            'id',
-            'name',
-            'price',
-        ]);
+        $source = new Storage\Snowflake\Table(
+            self::SNOWFLAKE_SOURCE_SCHEMA_NAME,
+            'nullify_src',
+            [
+                'id',
+                'name',
+                'price',
+            ]
+        );
+        $destination = new Storage\Snowflake\Table(self::SNOWFLAKE_DEST_SCHEMA_NAME, 'nullify');
 
         (new Importer($this->connection))->importTable(
             $source,
