7576e7de937146eec412f91506253065fc4d74f4
Martin Zajic <zajca@zajca.cz>
2021-01-15 14:07:07 +0100

    cs fix, line length

--- packages/php-db-import-export/provisioning/src/EnvCollector.php
+++ packages/php-db-import-export/provisioning/src/EnvCollector.php
@@ -21,6 +21,10 @@ final class EnvCollector
         if (!$fs->exists($this->envFile)) {
             $fs->touch($this->envFile);
         }
-        $fs->appendToFile($this->envFile, sprintf('echo "##vso[task.setvariable variable=%s]%s"' . PHP_EOL, $name, $value));
+        $fs->appendToFile($this->envFile, sprintf(
+            'echo "##vso[task.setvariable variable=%s]%s"' . PHP_EOL,
+            $name,
+            $value
+        ));
     }
 }
