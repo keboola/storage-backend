8d0a0d1ac65d01a89c39dac3b1536f434a3b3275
Martin Zajic <zajca@zajca.cz>
2021-01-28 15:45:02 +0100

    move TIMESTAMP_COLUMN_NAME to ImporterInterface

--- packages/php-db-import-export/src/Backend/Synapse/Importer.php
+++ packages/php-db-import-export/src/Backend/Synapse/Importer.php
@@ -16,8 +16,6 @@ use Keboola\Db\ImportExport\Storage;
 
 class Importer implements ImporterInterface
 {
-    public const TIMESTAMP_COLUMN_NAME = '_timestamp';
-
     public const DEFAULT_ADAPTERS = [
         Storage\ABS\SynapseImportAdapter::class,
         Storage\Synapse\SynapseImportAdapter::class,
