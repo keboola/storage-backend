76659ada656e44321e7d22187cc577d9583fb56c
zajca <zajca@zajca.cz>
2021-05-10 13:56:03 +0200

    add getCreateTableCommandFromDefinition to SynapseTableQueryBuilder

---
+++ packages/php-table-backend-utils/src/Escaping/SynapseQuote.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\TableBackendUtils\Escaping;
+
+class SynapseQuote implements QuoteInterface
+{
+    public static function quote(string $value): string
+    {
+        return "'" . str_replace("'", "''", $value) . "'";
+    }
+
+    public static function quoteSingleIdentifier(string $str): string
+    {
+        return '[' . str_replace(']', '][', $str) . ']';
+    }
+}
---
+++ packages/php-table-backend-utils/tests/Unit/Escaping/SynapseQuoteTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Keboola\TableBackendUtils\Unit\Escaping;
+
+use Keboola\TableBackendUtils\Escaping\SynapseQuote;
+use PHPUnit\Framework\TestCase;
+
+class SynapseQuoteTest extends TestCase
+{
+    /**
+     * @return \Generator<string, array<int, string>>
+     */
+    public function valueQuoteProvider(): \Generator
+    {
+        yield 'simple' => [
+            'valueToQuote',
+            '\'valueToQuote\'',
+        ];
+        yield 'with escaping char' => [
+            'value\'To\'Quote',
+            '\'value\'\'To\'\'Quote\'',
+        ];
+        yield 'complex' => [
+            'va[l.u]e\']To\'[Q][uo.t[]e',
+            '\'va[l.u]e\'\']To\'\'[Q][uo.t[]e\'',
+        ];
+    }
+
+    /**
+     * @dataProvider valueQuoteProvider
+     */
+    public function testQuote(string $value, string $expectedOutput): void
+    {
+        self::assertSame($expectedOutput, SynapseQuote::quote($value));
+    }
+
+    /**
+     * @return \Generator<string, array<int, string>>
+     */
+    public function valueQuoteIdentifierProvider(): \Generator
+    {
+        yield 'simple' => [
+            'valueToQuote',
+            '[valueToQuote]',
+        ];
+        yield 'with escaping char' => [
+            'value\'To\'Quote',
+            '[value\'To\'Quote]',
+        ];
+        yield 'complex' => [
+            'va[l.u]e\']To\'[Q][uo.t[]e',
+            '[va[l.u][e\'][To\'[Q][[uo.t[][e]',
+        ];
+    }
+
+
+    /**
+     * @dataProvider valueQuoteIdentifierProvider
+     */
+    public function testQuoteSingleIdentifier(string $value, string $expectedOutput): void
+    {
+        self::assertSame($expectedOutput, SynapseQuote::quoteSingleIdentifier($value));
+    }
+}
