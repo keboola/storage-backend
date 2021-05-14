70f14e6f182a50ee91f038bec09ec741c86ba201
zajca <zajca@zajca.cz>
2021-05-14 21:49:25 +0200

    c&p SynapseImportAdapter to new FromTableInsertIntoAdapter c&p SynapseAdapterTest to new FromTableInsertIntoAdapterTest

---
+++ packages/php-db-import-export/src/Backend/Synapse/ToStage/FromTableInsertIntoAdapter.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Db\ImportExport\Storage\Synapse;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\SQLServerPlatform;
+use Keboola\Db\ImportExport\Backend\Synapse\SqlCommandBuilder;
+use Keboola\Db\ImportExport\Backend\Synapse\SynapseImportAdapterInterface;
+use Keboola\Db\ImportExport\ImportOptionsInterface;
+use Keboola\Db\ImportExport\Storage;
+
+class SynapseImportAdapter implements SynapseImportAdapterInterface
+{
+    /** @var \Doctrine\DBAL\Platforms\AbstractPlatform|SQLServerPlatform */
+    private $platform;
+
+    /** @var Connection */
+    private $connection;
+
+    /** @var SqlCommandBuilder */
+    private $sqlBuilder;
+
+    public function __construct(Connection $connection)
+    {
+        $this->platform = $connection->getDatabasePlatform();
+        $this->connection = $connection;
+        $this->sqlBuilder = new SqlCommandBuilder($this->connection);
+    }
+
+    public static function isSupported(Storage\SourceInterface $source, Storage\DestinationInterface $destination): bool
+    {
+        if (!$destination instanceof Table) {
+            return false;
+        }
+
+        if (!$source instanceof Table && !$source instanceof SelectSource) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param Table|SelectSource $source
+     * @param Table $destination
+     */
+    public function runCopyCommand(
+        Storage\SourceInterface $source,
+        Storage\DestinationInterface $destination,
+        ImportOptionsInterface $importOptions,
+        string $stagingTableName
+    ): int {
+        $quotedColumns = array_map(function ($column) {
+            return $this->platform->quoteSingleIdentifier($column);
+        }, $source->getColumnsNames());
+
+        $sql = sprintf(
+            'INSERT INTO %s.%s (%s) %s',
+            $this->platform->quoteSingleIdentifier($destination->getSchema()),
+            $this->platform->quoteSingleIdentifier($stagingTableName),
+            implode(', ', $quotedColumns),
+            $source->getFromStatement()
+        );
+
+        if ($source instanceof SelectSource) {
+            $this->connection->executeQuery($sql, $source->getQueryBindings(), $source->getDataTypes());
+        } else {
+            $this->connection->exec($sql);
+        }
+
+        $rows = $this->connection->fetchAll($this->sqlBuilder->getTableItemsCountCommand(
+            $destination->getSchema(),
+            $stagingTableName
+        ));
+
+        return (int) $rows[0]['count'];
+    }
+}
---
+++ packages/php-db-import-export/tests/unit/Backend/Synapse/ToStage/FromTableInsertIntoAdapterTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Keboola\Db\ImportExportUnit\Storage\Synapse;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\SQLServer2012Platform;
+use Keboola\Db\ImportExport\ImportOptions;
+use Keboola\Db\ImportExport\Storage\Synapse\SynapseImportAdapter;
+use Keboola\Db\ImportExport\Storage;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tests\Keboola\Db\ImportExport\ABSSourceTrait;
+use Tests\Keboola\Db\ImportExportUnit\Backend\Synapse\MockConnectionTrait;
+use Tests\Keboola\Db\ImportExportUnit\BaseTestCase;
+
+class SynapseAdapterTest extends BaseTestCase
+{
+    use ABSSourceTrait;
+    use MockConnectionTrait;
+
+    public function testIsSupported(): void
+    {
+        $absSource = $this->createDummyABSSourceInstance('');
+        $snowflakeTable = new Storage\Snowflake\Table('', '');
+        $snowflakeSelectSource = new Storage\Snowflake\SelectSource('', []);
+        $synapseTable = new Storage\Synapse\Table('', '');
+
+        $this->assertTrue(
+            SynapseImportAdapter::isSupported(
+                $synapseTable,
+                $synapseTable
+            )
+        );
+
+        $this->assertFalse(
+            SynapseImportAdapter::isSupported(
+                $snowflakeSelectSource,
+                $snowflakeTable
+            )
+        );
+
+        $this->assertFalse(
+            SynapseImportAdapter::isSupported(
+                $absSource,
+                $snowflakeTable
+            )
+        );
+
+        $this->assertFalse(
+            SynapseImportAdapter::isSupported(
+                $snowflakeTable,
+                $synapseTable
+            )
+        );
+    }
+
+    public function testGetCopyCommands(): void
+    {
+        $source = new Storage\Synapse\Table('test_schema', 'test_table', ['col1', 'col2']);
+
+        $conn = $this->mockConnection();
+        $conn->expects($this->once())->method('exec')->with(
+        // phpcs:ignore
+            'INSERT INTO [test_schema].[stagingTable] ([col1], [col2]) SELECT [col1], [col2] FROM [test_schema].[test_table]'
+        );
+        $conn->expects($this->once())->method('fetchAll')
+            ->with('SELECT COUNT(*) AS [count] FROM [test_schema].[stagingTable]')
+            ->willReturn(
+                [
+                    [
+                        'count' => 10,
+                    ],
+                ]
+            );
+
+        $destination = new Storage\Synapse\Table('test_schema', 'test_table', ['col1', 'col2']);
+        $options = new ImportOptions([]);
+        $adapter = new SynapseImportAdapter($conn);
+        $count = $adapter->runCopyCommand(
+            $source,
+            $destination,
+            $options,
+            'stagingTable'
+        );
+
+        $this->assertEquals(10, $count);
+    }
+
+    public function testGetCopyCommandsSelectSource(): void
+    {
+        $source = new Storage\Synapse\SelectSource(
+            'SELECT * FROM [test_schema].[test_table]',
+            ['val'],
+            [1],
+            ['col1','col2']
+        );
+
+        $conn = $this->mockConnection();
+        $conn->expects($this->once())->method('executeQuery')->with(
+        // phpcs:ignore
+            'INSERT INTO [test_schema].[stagingTable] ([col1], [col2]) SELECT * FROM [test_schema].[test_table]',
+            ['val'],
+            [1]
+        );
+        $conn->expects($this->once())->method('fetchAll')
+            ->with('SELECT COUNT(*) AS [count] FROM [test_schema].[stagingTable]')
+            ->willReturn(
+                [
+                    [
+                        'count' => 10,
+                    ],
+                ]
+            );
+
+        $destination = new Storage\Synapse\Table('test_schema', 'test_table', ['col1', 'col2']);
+        $options = new ImportOptions([]);
+        $adapter = new SynapseImportAdapter($conn);
+        $count = $adapter->runCopyCommand(
+            $source,
+            $destination,
+            $options,
+            'stagingTable'
+        );
+
+        $this->assertEquals(10, $count);
+    }
+}
