b5ed90513b5884605881e36d884db9b3b4d45be3
zajca <zajca@zajca.cz>
2021-05-17 21:25:56 +0200

    drop unused constants

--- packages/php-db-import-export/src/Backend/ToFinalTableImporterInterface.php
+++ packages/php-db-import-export/src/Backend/ToFinalTableImporterInterface.php
@@ -10,9 +10,6 @@ use Keboola\TableBackendUtils\Table\TableDefinitionInterface;
 
 interface ToFinalTableImporterInterface
 {
-    public const TIMESTAMP_COLUMN_NAME = '_timestamp';
-    public const SLICED_FILES_CHUNK_SIZE = 1000;
-
     public function importToTable(
         TableDefinitionInterface $stagingTableDefinition,
         TableDefinitionInterface $destinationTableDefinition,
