b826bad789a2fa2e93b5d263da1749f16112df70
Jiří Semmler <jirka.semmler@gmail.com>
2021-07-22 18:50:11 +0200

    Exasol quoting

---
+++ packages/php-table-backend-utils/src/Escaping/Exasol/ExasolQuote.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\TableBackendUtils\Escaping\Exasol;
+
+use Keboola\TableBackendUtils\Escaping\QuoteInterface;
+
+class ExasolQuote implements QuoteInterface
+{
+    /**
+     * Exasol quotes strings by single quote
+     * uses '' for escaping
+     *
+     * @param string $value
+     * @return string
+     */
+    public static function quote(string $value): string
+    {
+        return "'" . str_replace("'", "''", $value) . "'";
+    }
+
+    /**
+     * Exasol quotes identifiers by double quote
+     *  - identifiers without "" are tranformed to UPPERCASE
+     *  - qouted identifiers are kept in desired form
+     * Query  | Exasol representation
+     * ------------------------------
+     *  AaA   | AAA
+     *  "AaA" | AaA
+     * uses "" for escaping
+     * @param string $str
+     * @return string
+     */
+    public static function quoteSingleIdentifier(string $str): string
+    {
+        return '"' . str_replace('"', '""', $str) . '"';
+    }
+}
