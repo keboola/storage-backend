fb34da2744e5c6c1ee0a4a58f54b2079b51720d9
Jiří Semmler <jirka.semmler@gmail.com>
2021-12-01 13:43:53 +0100

    KBC-2221 implement createFromDB in SnowflakeColumn

--- packages/php-table-backend-utils/src/Column/Snowflake/SnowflakeColumn.php
+++ packages/php-table-backend-utils/src/Column/Snowflake/SnowflakeColumn.php
@@ -55,8 +55,25 @@ final class SnowflakeColumn implements ColumnInterface
         return $this->columnDefinition;
     }
 
-    public static function createFromDB(array $dbResponse): ColumnInterface
+    public static function createFromDB(array $column): ColumnInterface
     {
-        // TODO: Implement createFromDB() method.
+        $type = $column['type'];
+        $default = $column['default'];
+        $length = null;
+
+        $matches = [];
+        if (preg_match('/^(\w+)\(([0-9\,]+)\)$/ui', $column['type'], $matches)) {
+            $type = $matches[1];
+            $length = $matches[2];
+        }
+
+        return new self($column['name'], new Snowflake(
+            $type,
+            [
+                'nullable' => $column['null?'] === 'Y' ? true : false,
+                'length' => $length,
+                'default' => $default,
+            ]
+        ));
     }
 }
--- packages/php-table-backend-utils/tests/Unit/Column/Snowflake/SnowflakeColumnTest.php
+++ packages/php-table-backend-utils/tests/Unit/Column/Snowflake/SnowflakeColumnTest.php
@@ -20,4 +20,54 @@ class SnowflakeColumnTest extends TestCase
         self::assertEquals('VARCHAR', $col->getColumnDefinition()->getType());
         self::assertEquals('\'\'', $col->getColumnDefinition()->getDefault());
     }
+
+    public function testCreateFromDB()
+    {
+        $data = [
+            "Row" => "1",
+            "name" => "first_name",
+            "type" => "VARCHAR(16777216)",
+            "kind" => "COLUMN",
+            "null?" => "Y",
+            "default" => "N",
+            "primary key" => "N",
+            "unique key" => "",
+            "check" => "",
+            "expression" => "",
+            "comment" => "",
+            "policy name" => "",
+        ];
+
+        $column = SnowflakeColumn::createFromDB($data);
+        self::assertEquals('first_name', $column->getColumnName());
+        self::assertEquals('VARCHAR(16777216)', $column->getColumnDefinition()->getSQLDefinition());
+        self::assertEquals('VARCHAR', $column->getColumnDefinition()->getType());
+        self::assertEquals('', $column->getColumnDefinition()->getDefault());
+        self::assertEquals('16777216', $column->getColumnDefinition()->getLength());
+    }
+
+    public function testCreateFromDBNotNullInt()
+    {
+        $data = [
+            "Row" => "1",
+            "name" => "age",
+            "type" => "NUMBER(38,0)",
+            "kind" => "COLUMN",
+            "null?" => "N",
+            "default" => "18",
+            "primary key" => "N",
+            "unique key" => "",
+            "check" => "",
+            "expression" => "",
+            "comment" => "",
+            "policy name" => "",
+        ];
+
+        $column = SnowflakeColumn::createFromDB($data);
+        self::assertEquals('age', $column->getColumnName());
+        self::assertEquals('NUMBER(38,0) NOT NULL', $column->getColumnDefinition()->getSQLDefinition());
+        self::assertEquals('NUMBER', $column->getColumnDefinition()->getType());
+        self::assertEquals('18', $column->getColumnDefinition()->getDefault());
+        self::assertEquals('38,0', $column->getColumnDefinition()->getLength());
+    }
 }
