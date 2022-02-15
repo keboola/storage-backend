4db51456e61f426c06d86e70ca7e358df31c7572
Roman Bracinik <roman.bracinik@keboola.com>
2022-02-15 14:50:44 +0000

    Add TeradataViewReflection

---
+++ packages/php-table-backend-utils/src/View/Teradata/TeradataViewReflection.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\TableBackendUtils\View\Teradata;
+
+use Doctrine\DBAL\Connection;
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+use Keboola\TableBackendUtils\View\ViewReflectionInterface;
+
+class TeradataViewReflection implements ViewReflectionInterface
+{
+    private Connection $connection;
+
+    private string $databaseName;
+
+    private string $viewName;
+
+    public function __construct(Connection $connection, string $databaseName, string $viewName)
+    {
+        $this->databaseName = $databaseName;
+        $this->viewName = $viewName;
+        $this->connection = $connection;
+    }
+
+    public function getDependentViews(): array
+    {
+        $views = $this->connection->fetchAllAssociative(
+            sprintf(
+                'SELECT * FROM dbc.tables WHERE tablekind=%s AND databasename=%s',
+                TeradataQuote::quote('V'),
+                TeradataQuote::quote($this->databaseName)
+            )
+        );
+
+        /**
+         * @var array{
+         *  schema_name: string,
+         *  name: string
+         * }[] $dependencies
+         */
+        $dependencies = [];
+        foreach ($views as $view) {
+            // trim table name from teradata, returned with whitespaces
+            $viewDefinition = $this->connection->fetchAllAssociative(
+                sprintf(
+                    'SHOW VIEW %s.%s',
+                    TeradataQuote::quoteSingleIdentifier($this->databaseName),
+                    TeradataQuote::quoteSingleIdentifier(trim($view['TableName']))
+                )
+            );
+
+            // trim table name from teradata, returned with whitespaces
+            $viewNameWithDatabase = sprintf(
+                '%s.%s',
+                TeradataQuote::quoteSingleIdentifier($this->databaseName),
+                TeradataQuote::quoteSingleIdentifier(trim($view['TableName']))
+            );
+
+            // remove create view statement
+            $text = str_replace(
+                'CREATE VIEW ' . $viewNameWithDatabase . ' AS SELECT * FROM',
+                '',
+                $viewDefinition[0]['Request Text']
+            );
+
+            $sourceNameWithDatabase = sprintf(
+                '%s.%s',
+                TeradataQuote::quoteSingleIdentifier($this->databaseName),
+                TeradataQuote::quoteSingleIdentifier($this->viewName)
+            );
+
+            if (strpos($text, $sourceNameWithDatabase) === false) {
+                continue;
+            }
+
+            // trim table name from teradata, returned with whitespaces
+            //return db in schema because teradata doesn't have schema
+            $dependencies[] = [
+                'schema_name' => trim($view['DatabaseName']),
+                'name' => trim($view['TableName']),
+            ];
+        }
+
+        return $dependencies;
+    }
+}
---
+++ packages/php-table-backend-utils/tests/Functional/View/Teradata/TeradataViewReflectionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Keboola\TableBackendUtils\Functional\View\Teradata;
+
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+use Keboola\TableBackendUtils\View\Teradata\TeradataViewReflection;
+use Tests\Keboola\TableBackendUtils\Functional\Teradata\TeradataBaseCase;
+
+/**
+ * @covers TeradataViewReflection
+ */
+class TeradataViewReflectionTest extends TeradataBaseCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanDatabase($this->getDatabaseName());
+        $this->createDatabase($this->getDatabaseName());
+    }
+
+    public function testGetDependentViews(): void
+    {
+        $this->initTable();
+        $this->initView();
+
+        $ref = new TeradataViewReflection($this->connection, $this->getDatabaseName(), self::VIEW_GENERIC);
+
+        self::assertCount(0, $ref->getDependentViews());
+
+        $secondViewName = self::VIEW_GENERIC . '-2';
+        $this->initView($secondViewName, self::VIEW_GENERIC);
+
+        $dependentViews = $ref->getDependentViews();
+
+        self::assertCount(1, $dependentViews);
+
+        self::assertSame([
+            'schema_name' => $this->getDatabaseName(),
+            'name' => $secondViewName,
+        ], $dependentViews[0]);
+    }
+
+    private function initView(
+        string $viewName = self::VIEW_GENERIC,
+        string $parentName = self::TABLE_GENERIC
+    ): void {
+        $this->connection->executeQuery(
+            sprintf(
+                'CREATE VIEW %s.%s AS SELECT * FROM %s.%s',
+                TeradataQuote::quoteSingleIdentifier($this->getDatabaseName()),
+                TeradataQuote::quoteSingleIdentifier($viewName),
+                TeradataQuote::quoteSingleIdentifier($this->getDatabaseName()),
+                TeradataQuote::quoteSingleIdentifier($parentName),
+            )
+        );
+    }
+}
