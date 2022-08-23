8014e770b54dbec8f21ddd16326e391af496d4ec
zajca <zajca@zajca.cz>
2022-08-23 16:12:03 +0200

    try remove casts, try remove coalesce from same tables

--- packages/php-db-import-export/tests/functional/Snowflake/ToFinal/SqlBuilderTest.php
+++ packages/php-db-import-export/tests/functional/Snowflake/ToFinal/SqlBuilderTest.php
@@ -205,12 +205,114 @@ class SqlBuilderTest extends SnowflakeBaseTestCase
 
         $sql = $this->getBuilder()->getDeleteOldItemsCommand(
             $stagingTableDefinition,
-            $tableDefinition
+            $tableDefinition,
+            $this->getSimpleImportOptions()
         );
 
         self::assertEquals(
         // phpcs:ignore
             'DELETE FROM "import_export_test_schema"."__temp_stagingTable" "src" USING "import_export_test_schema"."import_export_test_test" AS "dest" WHERE "dest"."pk1" = COALESCE("src"."pk1", \'\') AND "dest"."pk2" = COALESCE("src"."pk2", \'\') ',
+            $sql
+        );
+        $this->connection->executeStatement($sql);
+
+        $result = $this->connection->fetchAllAssociative(sprintf(
+            'SELECT * FROM %s',
+            $stagingTableSql
+        ));
+
+        self::assertCount(1, $result);
+        self::assertSame([
+            [
+                'pk1' => '2',
+                'pk2' => '1',
+                'col1' => '1',
+                'col2' => '1',
+            ],
+        ], $result);
+    }
+
+    public function testGetDeleteOldItemsCommandRequireSameTables(): void
+    {
+        $this->createTestSchema();
+
+        $tableDefinition = new SnowflakeTableDefinition(
+            self::TEST_SCHEMA,
+            self::TEST_TABLE,
+            false,
+            new ColumnCollection([
+                new SnowflakeColumn(
+                    'id',
+                    new Snowflake(
+                        Snowflake::TYPE_INT
+                    )
+                ),
+                SnowflakeColumn::createGenericColumn('pk1'),
+                SnowflakeColumn::createGenericColumn('pk2'),
+                SnowflakeColumn::createGenericColumn('col1'),
+                SnowflakeColumn::createGenericColumn('col2'),
+            ]),
+            ['pk1', 'pk2']
+        );
+        $tableSql = sprintf(
+            '%s.%s',
+            SnowflakeQuote::quoteSingleIdentifier($tableDefinition->getSchemaName()),
+            SnowflakeQuote::quoteSingleIdentifier($tableDefinition->getTableName())
+        );
+        $qb = new SnowflakeTableQueryBuilder();
+        $this->connection->executeStatement($qb->getCreateTableCommandFromDefinition($tableDefinition));
+        $this->connection->executeStatement(
+            sprintf(
+                'INSERT INTO %s("id","pk1","pk2","col1","col2") VALUES (1,1,1,\'1\',\'1\')',
+                $tableSql
+            )
+        );
+        $stagingTableDefinition = new SnowflakeTableDefinition(
+            self::TEST_SCHEMA,
+            self::TEST_STAGING_TABLE,
+            false,
+            new ColumnCollection([
+                SnowflakeColumn::createGenericColumn('pk1'),
+                SnowflakeColumn::createGenericColumn('pk2'),
+                SnowflakeColumn::createGenericColumn('col1'),
+                SnowflakeColumn::createGenericColumn('col2'),
+            ]),
+            ['pk1', 'pk2']
+        );
+        $this->connection->executeStatement($qb->getCreateTableCommandFromDefinition($stagingTableDefinition));
+        $stagingTableSql = sprintf(
+            '%s.%s',
+            SnowflakeQuote::quoteSingleIdentifier($stagingTableDefinition->getSchemaName()),
+            SnowflakeQuote::quoteSingleIdentifier($stagingTableDefinition->getTableName())
+        );
+        $this->connection->executeStatement(
+            sprintf(
+                'INSERT INTO %s("pk1","pk2","col1","col2") VALUES (1,1,\'1\',\'1\')',
+                $stagingTableSql
+            )
+        );
+        $this->connection->executeStatement(
+            sprintf(
+                'INSERT INTO %s("pk1","pk2","col1","col2") VALUES (2,1,\'1\',\'1\')',
+                $stagingTableSql
+            )
+        );
+
+        $sql = $this->getBuilder()->getDeleteOldItemsCommand(
+            $stagingTableDefinition,
+            $tableDefinition,
+            new SnowflakeImportOptions(
+                [],
+                false,
+                false,
+                0,
+                SnowflakeImportOptions::SAME_TABLES_REQUIRED //<- same tables are required
+            ),
+        );
+
+        self::assertEquals(
+        // phpcs:ignore
+            'DELETE FROM "import_export_test_schema"."__temp_stagingTable" "src" USING "import_export_test_schema"."import_export_test_test" AS "dest" WHERE "dest"."pk1" = "src"."pk1" AND "dest"."pk2" = "src"."pk2" ',
             $sql
         );
         $this->connection->executeStatement($sql);
@@ -312,7 +414,77 @@ EOT
 
         self::assertEquals(
         // phpcs:ignore
-            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2") (SELECT CAST(COALESCE("col1", \'\') AS VARCHAR (4000)) AS "col1",CAST(COALESCE("col2", \'\') AS VARCHAR (4000)) AS "col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
+            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2") (SELECT COALESCE("col1", \'\') AS "col1",COALESCE("col2", \'\') AS "col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
+            $sql
+        );
+
+        $out = $this->connection->executeStatement($sql);
+        self::assertEquals(4, $out);
+
+        $result = $this->connection->fetchAllAssociative(sprintf(
+            'SELECT * FROM %s',
+            self::TEST_TABLE_IN_SCHEMA
+        ));
+
+        self::assertEqualsCanonicalizing([
+            [
+                'id' => null,
+                'col1' => '1',
+                'col2' => '1',
+            ],
+            [
+                'id' => null,
+                'col1' => '1',
+                'col2' => '1',
+            ],
+            [
+                'id' => null,
+                'col1' => '2',
+                'col2' => '2',
+            ],
+            [
+                'id' => null,
+                'col1' => '',
+                'col2' => '',
+            ],
+        ], $result);
+    }
+
+    public function testGetInsertAllIntoTargetTableCommandSameTables(): void
+    {
+        $this->createTestSchema();
+        $destination = $this->createTestTableWithColumns();
+        $this->createStagingTableWithData(true);
+
+        // create fake stage and say that there is less columns
+        $fakeStage = new SnowflakeTableDefinition(
+            self::TEST_SCHEMA,
+            self::TEST_STAGING_TABLE,
+            true,
+            new ColumnCollection([
+                $this->createNullableGenericColumn('col1'),
+                $this->createNullableGenericColumn('col2'),
+            ]),
+            []
+        );
+
+        // no convert values no timestamp
+        $sql = $this->getBuilder()->getInsertAllIntoTargetTableCommand(
+            $fakeStage,
+            $destination,
+            new SnowflakeImportOptions(
+                [],
+                false,
+                false,
+                0,
+                SnowflakeImportOptions::SAME_TABLES_REQUIRED //<- same tables are required
+            ),
+            '2020-01-01 00:00:00'
+        );
+
+        self::assertEquals(
+        // phpcs:ignore
+            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2") (SELECT "col1","col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
             $sql
         );
 
@@ -430,7 +602,7 @@ EOT
         );
         self::assertEquals(
         // phpcs:ignore
-            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2") (SELECT IFF("col1" = \'\', NULL, "col1"),CAST(COALESCE("col2", \'\') AS VARCHAR (4000)) AS "col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
+            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2") (SELECT IFF("col1" = \'\', NULL, "col1"),COALESCE("col2", \'\') AS "col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
             $sql
         );
         $out = $this->connection->executeStatement($sql);
@@ -492,7 +664,7 @@ EOT
         );
         self::assertEquals(
         // phpcs:ignore
-            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2", "_timestamp") (SELECT IFF("col1" = \'\', NULL, "col1"),CAST(COALESCE("col2", \'\') AS VARCHAR (4000)) AS "col2",\'2020-01-01 00:00:00\' FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
+            'INSERT INTO "import_export_test_schema"."import_export_test_test" ("col1", "col2", "_timestamp") (SELECT IFF("col1" = \'\', NULL, "col1"),COALESCE("col2", \'\') AS "col2",\'2020-01-01 00:00:00\' FROM "import_export_test_schema"."__temp_stagingTable" AS "src")',
             $sql
         );
         $out = $this->connection->executeStatement($sql);
@@ -586,6 +758,88 @@ EOT
         self::assertEquals(
         // phpcs:ignore
             'UPDATE "import_export_test_schema"."import_export_test_test" AS "dest" SET "col1" = COALESCE("src"."col1", \'\'), "col2" = COALESCE("src"."col2", \'\') FROM "import_export_test_schema"."__temp_stagingTable" AS "src" WHERE "dest"."col1" = COALESCE("src"."col1", \'\')  AND (COALESCE(TO_VARCHAR("dest"."col1"), \'\') != COALESCE("src"."col1", \'\') OR COALESCE(TO_VARCHAR("dest"."col2"), \'\') != COALESCE("src"."col2", \'\'))',
+            $sql
+        );
+        $this->connection->executeStatement($sql);
+
+        $result = $this->connection->fetchAllAssociative(sprintf(
+            'SELECT * FROM %s',
+            self::TEST_TABLE_IN_SCHEMA
+        ));
+
+        self::assertEquals([
+            [
+                'id' => '1',
+                'col1' => '2',
+                'col2' => '2',
+            ],
+        ], $result);
+    }
+
+    public function testGetUpdateWithPkCommandRequireSameTables(): void
+    {
+        $this->createTestSchema();
+        $this->createTestTableWithColumns();
+        $this->createStagingTableWithData(true);
+        // create fake destination and say that there is pk on col1
+        $fakeDestination = new SnowflakeTableDefinition(
+            self::TEST_SCHEMA,
+            self::TEST_TABLE,
+            true,
+            new ColumnCollection([
+                $this->createNullableGenericColumn('col1'),
+                $this->createNullableGenericColumn('col2'),
+            ]),
+            ['col1']
+        );
+        // create fake stage and say that there is less columns
+        $fakeStage = new SnowflakeTableDefinition(
+            self::TEST_SCHEMA,
+            self::TEST_STAGING_TABLE,
+            true,
+            new ColumnCollection([
+                $this->createNullableGenericColumn('col1'),
+                $this->createNullableGenericColumn('col2'),
+            ]),
+            []
+        );
+
+        $this->connection->executeStatement(
+            sprintf(
+                'INSERT INTO %s("id","col1","col2") VALUES (1,\'2\',\'1\')',
+                self::TEST_TABLE_IN_SCHEMA
+            )
+        );
+
+        $result = $this->connection->fetchAllAssociative(sprintf(
+            'SELECT * FROM %s',
+            self::TEST_TABLE_IN_SCHEMA
+        ));
+
+        self::assertEquals([
+            [
+                'id' => '1',
+                'col1' => '2',
+                'col2' => '1',
+            ],
+        ], $result);
+
+        // no convert values no timestamp
+        $sql = $this->getBuilder()->getUpdateWithPkCommand(
+            $fakeStage,
+            $fakeDestination,
+            new SnowflakeImportOptions(
+                [],
+                false,
+                false,
+                0,
+                SnowflakeImportOptions::SAME_TABLES_REQUIRED //<- same tables are required
+            ),
+            '2020-01-01 00:00:00'
+        );
+        self::assertEquals(
+        // phpcs:ignore
+            'UPDATE "import_export_test_schema"."import_export_test_test" AS "dest" SET "col1" = "src"."col1", "col2" = "src"."col2" FROM "import_export_test_schema"."__temp_stagingTable" AS "src" WHERE "dest"."col1" = "src"."col1"  AND ("dest"."col1" != "src"."col1" OR "dest"."col2" != "src"."col2")',
             $sql
         );
         $this->connection->executeStatement($sql);
