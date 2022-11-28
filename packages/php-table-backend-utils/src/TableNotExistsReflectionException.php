d57cca75dd425efc4433a6dab95858f41bda7a30
zajca <zajca@zajca.cz>
2022-11-28 09:29:51 +0100

    SnowflakeTableReflection now works with views

--- packages/php-table-backend-utils/src/TableNotExistsReflectionException.php
+++ packages/php-table-backend-utils/src/TableNotExistsReflectionException.php
@@ -6,4 +6,20 @@ namespace Keboola\TableBackendUtils;
 
 class TableNotExistsReflectionException extends ReflectionException
 {
+    /**
+     * @param string[] $path
+     */
+    public static function createForTable(array $path): self
+    {
+        return new self(sprintf(
+            'Table %s does not exists.',
+            implode(
+                ',',
+                array_map(
+                    fn(string $item) => sprintf('"%s"', $item),
+                    $path
+                )
+            )
+        ));
+    }
 }
