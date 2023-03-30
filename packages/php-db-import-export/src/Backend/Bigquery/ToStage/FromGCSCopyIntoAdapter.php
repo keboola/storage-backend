6e4591a173bfc0addbe5ec737ecdbc429a8c682a
Roman Bracinik <roman.bracinik@keboola.com>
2023-03-30 15:09:56 +0200

    Use BQ methods instead of query

--- packages/php-db-import-export/src/Backend/Bigquery/ToStage/FromGCSCopyIntoAdapter.php
+++ packages/php-db-import-export/src/Backend/Bigquery/ToStage/FromGCSCopyIntoAdapter.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Keboola\Db\ImportExport\Backend\Bigquery\ToStage;
 
+use Exception;
 use Google\Cloud\BigQuery\BigQueryClient;
-use Keboola\Db\ImportExport\Backend\Bigquery\Helper\CopyCommandCsvOptionsHelper;
+use Google\Cloud\Core\Exception\BadRequestException;
 use Keboola\Db\ImportExport\Backend\CopyAdapterInterface;
 use Keboola\Db\ImportExport\ImportOptions;
 use Keboola\Db\ImportExport\ImportOptionsInterface;
 use Keboola\Db\ImportExport\Storage;
-use Keboola\TableBackendUtils\Escaping\Bigquery\BigqueryQuote;
 use Keboola\TableBackendUtils\Table\Bigquery\BigqueryTableDefinition;
 use Keboola\TableBackendUtils\Table\Bigquery\BigqueryTableReflection;
 use Keboola\TableBackendUtils\Table\TableDefinitionInterface;
@@ -33,33 +33,44 @@ class FromGCSCopyIntoAdapter implements CopyAdapterInterface
         assert($destination instanceof BigqueryTableDefinition);
         assert($importOptions instanceof ImportOptions);
 
-        $destinationSchema = BigqueryQuote::quoteSingleIdentifier($destination->getSchemaName());
-        $destinationTable = BigqueryQuote::quoteSingleIdentifier($destination->getTableName());
-
         $entries = $source->getManifestEntries(Storage\GCS\SourceFile::PROTOCOL_GS);
 
         if (count($entries) === 0) {
             return 0;
         }
 
-        $entries = array_map(function ($entry) {
-            return BigqueryQuote::quote($entry);
-        }, $entries);
+        $schema = $this->bqClient->dataset($destination->getSchemaName());
 
-        $entries = implode(', ', $entries);
+        if (!$schema->exists()) {
+            throw new Exception(sprintf('Schema "%s" does not exist', $destination->getSchemaName()));
+        }
 
-        $options = CopyCommandCsvOptionsHelper::getCsvCopyCommandOptions($importOptions, $source->getCsvOptions());
+        $table = $schema->table($destination->getTableName());
 
-        $sql = sprintf(
-            'LOAD DATA INTO %s.%s FROM FILES (%s, format = \'CSV\', uris = [%s]);',
-            $destinationSchema,
-            $destinationTable,
-            implode(',', $options),
-            $entries
-        );
+        if (!$table->exists()) {
+            throw new Exception(sprintf('Table "%s" does not exist', $destination->getTableName()));
+        }
 
-        $query = $this->bqClient->query($sql);
-        $this->bqClient->runQuery($query);
+        // parameter is no nullable, but when I set first entry and removed it from array file won't import
+        // so this only possible solution
+        $loadConfig = $table->loadFromStorage(reset($entries))
+            ->sourceFormat('CSV')
+            ->sourceUris($entries)
+            ->autodetect(false)
+            ->fieldDelimiter($source->getCsvOptions()->getDelimiter())
+            ->skipLeadingRows($importOptions->getNumberOfIgnoredLines())
+            ->quote($source->getCsvOptions()->getEnclosure())
+            ->allowQuotedNewlines(true);
+        $job = $this->bqClient->runJob($loadConfig);
+
+        if (!$job->isComplete()) {
+            throw new Exception('Job has not yet completed');
+        }
+        // check if the job has errors
+        if (isset($job->info()['status']['errorResult'])) {
+            $error = $job->info()['status']['errorResult']['message'];
+            throw new BadRequestException($error);
+        }
 
         $ref = new BigqueryTableReflection(
             $this->bqClient,
