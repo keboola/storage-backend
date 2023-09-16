97087fb75e0adfa385520bf1aaa81b31d7b67669
Roman Bracinik <roman.bracinik@keboola.com>
2023-09-16 14:20:23 +0200

    Add runtime options to client interface

--- packages/php-storage-driver-common/contract/Driver/ClientInterface.php
+++ packages/php-storage-driver-common/contract/Driver/ClientInterface.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace Keboola\StorageDriver\Contract\Driver;
 
 use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Common\RuntimeOptions;
 
 interface ClientInterface
 {
     /**
      * @param string[] $features
+     * @param RuntimeOptions $runtimeOptions
      */
     public function runCommand(
         Message $credentials,
         Message $command,
-        array $features
+        array $features,
+        Message $runtimeOptions
     ): ?Message;
 }
