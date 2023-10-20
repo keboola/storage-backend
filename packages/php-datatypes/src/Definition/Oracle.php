f74d53accb32a98fce996e60943c755762208ad3
Výborný Adam <mail@adamvyborny.cz>
2023-10-20 10:50:22 +0200

    Removed lengths and use BaseType constants

--- packages/php-datatypes/src/Definition/Oracle.php
+++ packages/php-datatypes/src/Definition/Oracle.php
@@ -48,11 +48,6 @@ class Oracle extends Common
         self::TYPE_BLOB,
     ];
 
-    public const MAX_VARCHAR2_LENGTH = 4000;
-    public const MAX_CHAR_LENGTH = 2000;
-    public const MAX_RAW_LENGTH = 2000;
-    public const MAX_NUMBER_PRECISION = 38;
-
     /**
      * @param array{length?:string|null, nullable?:bool, default?:string|null} $options
      * @throws InvalidOptionException
@@ -62,7 +57,6 @@ class Oracle extends Common
     public function __construct(string $type, array $options = [])
     {
         $this->validateType($type);
-        $this->validateLength($type, $options['length'] ?? null);
         $diff = array_diff(array_keys($options), ['length', 'nullable', 'default']);
         if ($diff !== []) {
             throw new InvalidOptionException("Option '{$diff[0]}' not supported");
@@ -99,10 +93,10 @@ class Oracle extends Common
     public function getBasetype(): string
     {
         return match ($this->type) {
-            self::TYPE_VARCHAR2, self::TYPE_CHAR, self::TYPE_CLOB => 'string',
-            self::TYPE_NUMBER, self::TYPE_BINARY_FLOAT, self::TYPE_BINARY_DOUBLE => 'numeric',
-            self::TYPE_DATE, self::TYPE_TIMESTAMP, self::TYPE_TIMESTAMP_LTZ => 'datetime',
-            self::TYPE_RAW, self::TYPE_BLOB => 'binary',
+            self::TYPE_VARCHAR2, self::TYPE_CHAR, self::TYPE_CLOB, self::TYPE_RAW, self::TYPE_BLOB => BaseType::STRING,
+            self::TYPE_NUMBER, self::TYPE_BINARY_FLOAT, self::TYPE_BINARY_DOUBLE => BaseType::NUMERIC,
+            // DATE in Oracle includes time, so it is mapped to TIMESTAMP
+            self::TYPE_DATE, self::TYPE_TIMESTAMP, self::TYPE_TIMESTAMP_LTZ => BaseType::TIMESTAMP,
             default => throw new InvalidTypeException(sprintf('No base type mapped for type "%s"', $this->type)),
         };
     }
@@ -126,10 +120,9 @@ class Oracle extends Common
     public static function getTypeByBasetype(string $basetype): string
     {
         return match ($basetype) {
-            'string' => self::TYPE_VARCHAR2,
-            'numeric' => self::TYPE_NUMBER,
-            'datetime' => self::TYPE_DATE,
-            'binary' => self::TYPE_BLOB,
+            BaseType::STRING => self::TYPE_VARCHAR2,
+            BaseType::NUMERIC, BaseType::FLOAT, BaseType::BOOLEAN, BaseType::INTEGER => self::TYPE_NUMBER,
+            BaseType::TIMESTAMP, BaseType::DATE => self::TYPE_DATE,
             default => throw new InvalidTypeException(
                 sprintf('No Oracle type mapped for base type "%s"', $basetype)
             ),
@@ -143,57 +136,6 @@ class Oracle extends Common
     {
         if (!in_array(strtoupper($type), self::TYPES, true)) {
             throw new InvalidTypeException(sprintf('"%s" is not a valid type', $type));
-        }
-    }
-
-    /**
-     * Validate length based on the type.
-     *
-     * @param int|string|null $length
-     * @throws \Keboola\Datatype\Definition\Exception\InvalidLengthException
-     */
-    //phpcs:ignore SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
-    private function validateLength(string $type, $length): void
-    {
-        switch ($type) {
-            case self::TYPE_VARCHAR2:
-                if (!$this->validateMaxLength($length, self::MAX_VARCHAR2_LENGTH)) {
-                    throw new InvalidLengthException(sprintf(
-                        'Invalid length for type "%s". Allowed maximum length is %d.',
-                        $type,
-                        self::MAX_VARCHAR2_LENGTH
-                    ));
-                }
-                break;
-
-            case self::TYPE_RAW:
-            case self::TYPE_CHAR:
-                if (!$this->validateMaxLength($length, self::MAX_CHAR_LENGTH)) {
-                    throw new InvalidLengthException(sprintf(
-                        'Invalid length for type "%s". Allowed maximum length is %d.',
-                        $type,
-                        self::MAX_CHAR_LENGTH
-                    ));
-                }
-                break;
-
-            case self::TYPE_NUMBER:
-                if (!$this->validateNumericLength($length, self::MAX_NUMBER_PRECISION, self::MAX_NUMBER_PRECISION)) {
-                    throw new InvalidLengthException(sprintf(
-                        'Invalid length for type "%s". Precision can be up to %d.',
-                        $type,
-                        self::MAX_NUMBER_PRECISION
-                    ));
-                }
-                break;
-
-            default:
-                if ($length !== null) {
-                    throw new InvalidLengthException(
-                        sprintf('Length specification is not supported for type "%s".', $type)
-                    );
-                }
-                break;
         }
     }
 }
--- packages/php-datatypes/tests/OracleDatatypeTest.php
+++ packages/php-datatypes/tests/OracleDatatypeTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Keboola\DatatypeTest;
 
 use Generator;
-use Keboola\Datatype\Definition\Exception\InvalidLengthException;
+use Keboola\Datatype\Definition\BaseType;
 use Keboola\Datatype\Definition\Exception\InvalidOptionException;
 use Keboola\Datatype\Definition\Exception\InvalidTypeException;
 use Keboola\Datatype\Definition\Oracle;
@@ -31,40 +31,21 @@ class OracleDatatypeTest extends TestCase
         $this->assertEquals(Oracle::TYPE_NUMBER, $datatype->toArray()['type']);
     }
 
-    /**
-     * @dataProvider invalidLengthsProvider
-     */
-    public function testInvalidLengths(string $type, int|string|null $length): void
-    {
-        $this->expectException(InvalidLengthException::class);
-        new Oracle($type, ['length' => $length]);
-    }
-
-    /**
-     * @return \Generator<string, array<string, int|string>>
-     */
-    public function invalidLengthsProvider(): Generator
-    {
-        yield Oracle::TYPE_CHAR => ['type' => Oracle::TYPE_CHAR, 'length' => 2500];
-        yield Oracle::TYPE_RAW => ['type' => Oracle::TYPE_RAW, 'length' => 2500];
-        yield Oracle::TYPE_NUMBER => ['type' => Oracle::TYPE_NUMBER, 'length' => '40,2'];
-    }
-
     public function testBasetypes(): void
     {
         foreach (Oracle::TYPES as $type) {
             $datatype = new Oracle($type);
             $basetype = $datatype->getBasetype();
-            $this->assertContains($basetype, ['string', 'numeric', 'datetime', 'binary']);
+            $this->assertContains($basetype, BaseType::TYPES);
         }
     }
 
     public function testGetTypeByBasetype(): void
     {
-        $this->assertEquals(Oracle::TYPE_VARCHAR2, Oracle::getTypeByBasetype('string'));
-        $this->assertEquals(Oracle::TYPE_NUMBER, Oracle::getTypeByBasetype('numeric'));
-        $this->assertEquals(Oracle::TYPE_DATE, Oracle::getTypeByBasetype('datetime'));
-        $this->assertEquals(Oracle::TYPE_BLOB, Oracle::getTypeByBasetype('binary'));
+        foreach (BaseType::TYPES as $basetype) {
+            $type = Oracle::getTypeByBasetype($basetype);
+            $this->assertContains($type, Oracle::TYPES);
+        }
     }
 
     public function testGetSQLDefinition(): void
