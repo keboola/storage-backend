afa34a1ca60fa12859a1bd0e06fe75c237038fe9
Vojta Biberle <vojtech.biberle@gmail.com>
2025-01-17 19:33:27 +0100

    DMD-33 snowflake definitions directly from SchemaReflection

--- packages/php-table-backend-utils/src/Schema/Snowflake/SnowflakeSchemaReflection.php
+++ packages/php-table-backend-utils/src/Schema/Snowflake/SnowflakeSchemaReflection.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace Keboola\TableBackendUtils\Schema\Snowflake;
 
 use Doctrine\DBAL\Connection;
-use Keboola\TableBackendUtils\DataHelper;
+use Keboola\TableBackendUtils\Column\ColumnCollection;
+use Keboola\TableBackendUtils\Column\Snowflake\SnowflakeColumn;
 use Keboola\TableBackendUtils\Escaping\Snowflake\SnowflakeQuote;
 use Keboola\TableBackendUtils\Schema\SchemaReflectionInterface;
+use Keboola\TableBackendUtils\Table\Snowflake\SnowflakeTableDefinition;
+use Keboola\TableBackendUtils\Table\TableType;
+use RuntimeException;
 
 final class SnowflakeSchemaReflection implements SchemaReflectionInterface
 {
@@ -51,5 +55,91 @@ final class SnowflakeSchemaReflection implements SchemaReflectionInterface
         );
 
         return array_map(static fn($table) => $table['name'], $tables);
+    }
+
+    /**
+     * @return array<int, SnowflakeTableDefinition>
+     */
+    public function getDefinitions(): array
+    {
+        $informationsQuery = sprintf(
+            'SELECT TABLE_NAME, TABLE_TYPE, BYTES, ROW_COUNT '.
+            'FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = \'%s\' ORDER BY TABLE_NAME;',
+            $this->schemaName,
+        );
+
+        $columnsQuery = sprintf(
+            'SELECT TABLE_NAME, '.
+            'COLUMN_NAME AS "name", DATA_TYPE AS "type", COLUMN_DEFAULT AS "default", IS_NULLABLE AS "null?" '.
+            'FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = \'%s\' ORDER BY TABLE_NAME;',
+            $this->schemaName,
+        );
+
+        $primaryKeyQuery = sprintf(
+            'SHOW PRIMARY KEYS',
+        );
+
+        /** @var array<int, array{TABLE_NAME: string, TABLE_TYPE: string, BYTES: int, ROW_COUNT: int}> $informations */
+        $informations = $this->connection->fetchAllAssociative($informationsQuery);
+        /** @var array<int, array{TABLE_NAME: string, name: string, type: string, default: string, null?: string}> $columns */
+        $columns = $this->connection->fetchAllAssociative($columnsQuery);
+        /** @var array<int, array{TABLE_NAME: string, COLUMN_NAME: string}> $primaryKeys */
+        $primaryKeys = $this->connection->fetchAllAssociative($primaryKeyQuery);
+
+        $tables = [];
+
+        foreach ($informations as $information) {
+            $tables[$information['TABLE_NAME']]['PROPS'] = $information;
+
+            switch (strtoupper($information['TABLE_TYPE'])) {
+                case 'BASE TABLE':
+                    $tables[$information['TABLE_NAME']]['PROPS']['TEMPORARY'] = false;
+                    $tables[$information['TABLE_NAME']]['PROPS']['TABLE_TYPE'] = TableType::TABLE;
+                    break;
+                case 'EXTERNAL TABLE':
+                    $tables[$information['TABLE_NAME']]['PROPS']['TEMPORARY'] = false;
+                    $tables[$information['TABLE_NAME']]['PROPS']['TABLE_TYPE'] = TableType::SNOWFLAKE_EXTERNAL;
+                    break;
+                case 'LOCAL TEMPORARY':
+                case 'TEMPORARY TABLE':
+                    $tables[$information['TABLE_NAME']]['PROPS']['TEMPORARY'] = true;
+                    $tables[$information['TABLE_NAME']]['PROPS']['TABLE_TYPE'] = TableType::TABLE;
+                    break;
+                case 'VIEW':
+                    $tables[$information['TABLE_NAME']]['PROPS']['TEMPORARY'] = false;
+                    $tables[$information['TABLE_NAME']]['PROPS']['TABLE_TYPE'] = TableType::VIEW;
+                    break;
+                default:
+                    throw new RuntimeException(sprintf(
+                        'Table type "%s" is not known.',
+                        $information['TABLE_TYPE'],
+                    ));
+            }
+        }
+
+        foreach ($columns as $column) {
+            // Offset 'null?' does not exist on
+            // array{TABLE_NAME: string, name: string, type: string, default: string, null?: string}.
+            // @phpstan-ignore-next-line
+            $column['null?'] = ($column['null?'] === 'YES' ? 'Y' : 'N');
+            $tables[$column['TABLE_NAME']]['COLUMNS'][] = SnowflakeColumn::createFromDB($column);
+        }
+
+        foreach ($primaryKeys as $primaryKey) {
+            $tables[$primaryKey['TABLE_NAME']]['PRIMARY_KEYS'][] = $primaryKey['COLUMN_NAME'];
+        }
+
+        $definitions = [];
+        foreach ($tables as $tableName => $table) {
+            $definitions[] = new SnowflakeTableDefinition(
+                $this->schemaName,
+                $tableName,
+                $table['PROPS']['TEMPORARY'],
+                new ColumnCollection($table['COLUMNS'] ?? []),
+                $table['PRIMARY_KEYS'] ?? [],
+                $table['PROPS']['TABLE_TYPE'],
+            );
+        }
+        return $definitions;
     }
 }
--- packages/php-table-backend-utils/tests/Functional/Snowflake/Schema/SnowflakeSchemaReflectionTest.php
+++ packages/php-table-backend-utils/tests/Functional/Snowflake/Schema/SnowflakeSchemaReflectionTest.php
@@ -7,6 +7,7 @@ namespace Tests\Keboola\TableBackendUtils\Functional\Snowflake\Schema;
 use Keboola\TableBackendUtils\Escaping\Snowflake\SnowflakeQuote;
 use Keboola\TableBackendUtils\Schema\Snowflake\SnowflakeSchemaReflection;
 use Tests\Keboola\TableBackendUtils\Functional\Snowflake\SnowflakeBaseCase;
+use function PHPUnit\Framework\assertEquals;
 
 class SnowflakeSchemaReflectionTest extends SnowflakeBaseCase
 {
@@ -77,5 +78,81 @@ CREATE VIEW %s.%s AS
         );
         $this->connection->executeQuery($sql);
         self::assertSame([$viewName], $this->schemaRef->getViewsNames());
+    }
+
+    public function testGetDefinitions(): void
+    {
+        $this->initTable();
+
+        // create transient table
+        $this->connection->executeQuery(
+            sprintf(
+                'CREATE OR REPLACE TRANSIENT TABLE %s.%s (
+            "id" INTEGER,
+    "first_name" VARCHAR(100),
+    "last_name" VARCHAR(100)
+);',
+                SnowflakeQuote::quoteSingleIdentifier(self::TEST_SCHEMA),
+                SnowflakeQuote::quoteSingleIdentifier('transient_table'),
+            ),
+        );
+
+        // create temporary table
+        $this->connection->executeQuery(
+            sprintf(
+                'CREATE OR REPLACE TEMPORARY TABLE %s.%s (
+            "id" INTEGER,
+    "first_name" VARCHAR(100),
+    "last_name" VARCHAR(100)
+);',
+                SnowflakeQuote::quoteSingleIdentifier(self::TEST_SCHEMA),
+                SnowflakeQuote::quoteSingleIdentifier('temporary_table'),
+            ),
+        );
+
+        $tableName = self::TABLE_GENERIC;
+        $schemaName = self::TEST_SCHEMA;
+        $viewName = self::VIEW_GENERIC;
+        $sql = sprintf(
+            '
+CREATE VIEW %s.%s AS
+     SELECT   "first_name",
+              "last_name" 
+     FROM %s.%s;
+',
+            SnowflakeQuote::quoteSingleIdentifier($schemaName),
+            SnowflakeQuote::quoteSingleIdentifier($viewName),
+            SnowflakeQuote::quoteSingleIdentifier($schemaName),
+            SnowflakeQuote::quoteSingleIdentifier($tableName),
+        );
+        $this->connection->executeQuery($sql);
+
+        $definitions = $this->schemaRef->getDefinitions();
+
+        self::assertCount(4, $definitions);
+
+        $temporaryTableIndex = 0;
+        self::assertEquals('temporary_table', $definitions[$temporaryTableIndex]->getTableName());
+        self::assertEquals(3, $definitions[$temporaryTableIndex]->getColumnsDefinitions()->count());
+        self::assertTrue($definitions[$temporaryTableIndex]->isTemporary());
+        self::assertEquals('table', $definitions[$temporaryTableIndex]->getTableType()->value);
+
+        $transientTableIndex = 1;
+        self::assertEquals('transient_table', $definitions[$transientTableIndex]->getTableName());
+        self::assertEquals(3, $definitions[$transientTableIndex]->getColumnsDefinitions()->count());
+        self::assertFalse($definitions[$transientTableIndex]->isTemporary());
+        self::assertEquals('table', $definitions[$transientTableIndex]->getTableType()->value);
+
+        $genericTableIndex = 2;
+        self::assertEquals(self::TABLE_GENERIC, $definitions[$genericTableIndex]->getTableName());
+        self::assertEquals(3, $definitions[$genericTableIndex]->getColumnsDefinitions()->count());
+        self::assertFalse($definitions[$genericTableIndex]->isTemporary());
+        self::assertEquals('table', $definitions[$genericTableIndex]->getTableType()->value);
+
+        $genericViewIndex = 3;
+        self::assertEquals(self::VIEW_GENERIC, $definitions[$genericViewIndex]->getTableName());
+        self::assertEquals(2, $definitions[$genericViewIndex]->getColumnsDefinitions()->count());
+        self::assertFalse($definitions[$genericViewIndex]->isTemporary());
+        self::assertEquals('view', $definitions[$genericViewIndex]->getTableType()->value);
     }
 }
