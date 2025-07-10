9a5e242abd4e3522fa9042715be4f2cb6f235313
Roman Bracinik <roman.bracinik@keboola.com>
2025-07-10 12:30:38 +0200

    init

---
+++ packages/php-db-import-export/src/Backend/Snowflake/Export/S3ParquetExportAdapter.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Db\ImportExport\Backend\Snowflake\Export;
+
+use Doctrine\DBAL\Connection;
+use Keboola\Db\ImportExport\Backend\BackendExportAdapterInterface;
+use Keboola\Db\ImportExport\ExportOptions;
+use Keboola\Db\ImportExport\ExportOptionsInterface;
+use Keboola\Db\ImportExport\Storage;
+
+class S3ParquetExportAdapter implements BackendExportAdapterInterface
+{
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public static function isSupported(Storage\SourceInterface $source, Storage\DestinationInterface $destination): bool
+    {
+        if (!$source instanceof Storage\SqlSourceInterface) {
+            return false;
+        }
+        if (!$destination instanceof Storage\S3\DestinationFile) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param Storage\SqlSourceInterface $source
+     * @param Storage\S3\DestinationFile $destination
+     * @param ExportOptions $exportOptions
+     * @return array<mixed>
+     */
+    public function runCopyCommand(
+        Storage\SourceInterface $source,
+        Storage\DestinationInterface $destination,
+        ExportOptionsInterface $exportOptions,
+    ): array {
+        $sql = sprintf(
+            <<<EOT
+COPY INTO '%s/%s'
+FROM (%s)
+CREDENTIALS = (
+    AWS_KEY_ID = '%s'
+    AWS_SECRET_KEY = '%s'
+)
+REGION = '%s'
+FILE_FORMAT = (
+    TYPE = 'PARQUET'
+    %s
+)
+ENCRYPTION = (
+    TYPE = 'AWS_SSE_S3'
+)
+OVERWRITE = TRUE
+MAX_FILE_SIZE = %d
+DETAILED_OUTPUT = TRUE
+EOT,
+            $destination->getS3Prefix(),
+            $destination->getFilePath(),
+            $source->getFromStatement(),
+            $destination->getKey(),
+            $destination->getSecret(),
+            $destination->getRegion(),
+            $exportOptions->isCompressed() ? "COMPRESSION='SNAPPY'" : "COMPRESSION='NONE'",
+            Exporter::DEFAULT_SLICE_SIZE,
+        );
+
+        $this->connection->executeQuery($sql, $source->getQueryBindings());
+
+        /** @var array<array{FILE_NAME: string, FILE_SIZE: string, ROW_COUNT: string}> $unloadedFiles */
+        $unloadedFiles = $this->connection->fetchAllAssociative('select * from table(result_scan(last_query_id()));');
+
+        if ($exportOptions->generateManifest()) {
+            (new Storage\S3\ManifestGenerator\S3SlicedManifestFromUnloadQueryResultGenerator($destination->getClient()))
+                ->generateAndSaveManifest($destination->getRelativePath(), $unloadedFiles);
+        }
+
+        return $unloadedFiles;
+    }
+}
---
+++ packages/php-db-import-export/tests/unit/Backend/Snowflake/Exporter/S3ParquetExportAdapterTest.php
@@ -0,0 +1,202 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Keboola\Db\ImportExportUnit\Backend\Snowflake\Exporter;
+
+use Doctrine\DBAL\Connection;
+use Keboola\Db\ImportExport\Backend\Snowflake\Export\S3ParquetExportAdapter;
+use Keboola\Db\ImportExport\Backend\Snowflake\Export\Exporter;
+use Keboola\Db\ImportExport\ExportOptions;
+use Keboola\Db\ImportExport\Storage;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tests\Keboola\Db\ImportExportUnit\BaseTestCase;
+
+class S3ParquetExportAdapterTest extends BaseTestCase
+{
+    public function testGetCopyCommand(): void
+    {
+        $expectedCopyResult = [
+            [
+                'FILE_NAME' => 'containerUrl',
+                'FILE_SIZE' => '0',
+                'ROW_COUNT' => '123',
+            ],
+        ];
+
+        /** @var Storage\S3\DestinationFile|MockObject $destination */
+        $destination = $this->createMock(Storage\S3\DestinationFile::class);
+        $destination->expects(self::once())->method('getFilePath')->willReturn('xxx/path');
+        $destination->expects(self::once())->method('getKey')->willReturn('key');
+        $destination->expects(self::once())->method('getSecret')->willReturn('secret');
+        $destination->expects(self::once())->method('getRegion')->willReturn('region');
+        $destination->expects(self::once())->method('getS3Prefix')->willReturn('s3://bucketUrl');
+
+        /** @var Connection|MockObject $conn */
+        $conn = $this->createMock(Connection::class);
+        $conn->expects(self::once())->method('executeQuery')->with(
+            <<<EOT
+COPY INTO 's3://bucketUrl/xxx/path'
+FROM (SELECT * FROM "schema"."table")
+CREDENTIALS = (
+    AWS_KEY_ID = 'key'
+    AWS_SECRET_KEY = 'secret'
+)
+REGION = 'region'
+FILE_FORMAT = (
+    TYPE = 'PARQUET'
+    COMPRESSION='NONE'
+)
+ENCRYPTION = (
+    TYPE = 'AWS_SSE_S3'
+)
+OVERWRITE = TRUE
+MAX_FILE_SIZE = 200000000
+DETAILED_OUTPUT = TRUE
+EOT
+            ,
+            [],
+        );
+
+        $conn->expects(self::once())->method('fetchAllAssociative')
+            ->with('select * from table(result_scan(last_query_id()));')
+            ->willReturn($expectedCopyResult);
+
+        $source = new Storage\Snowflake\Table('schema', 'table');
+        $options = new ExportOptions(false, ExportOptions::MANIFEST_SKIP);
+        $adapter = new S3ParquetExportAdapter($conn);
+
+        $this->assertSame(
+            $expectedCopyResult,
+            $adapter->runCopyCommand(
+                $source,
+                $destination,
+                $options,
+            ),
+        );
+    }
+
+    public function testGetCopyCommandCompressed(): void
+    {
+        $expectedCopyResult = [
+            [
+                'FILE_NAME' => 'containerUrl',
+                'FILE_SIZE' => '0',
+                'ROW_COUNT' => '123',
+            ],
+        ];
+
+        /** @var Storage\S3\DestinationFile|MockObject $destination */
+        $destination = $this->createMock(Storage\S3\DestinationFile::class);
+        $destination->expects(self::once())->method('getFilePath')->willReturn('xxx/path');
+        $destination->expects(self::once())->method('getKey')->willReturn('key');
+        $destination->expects(self::once())->method('getSecret')->willReturn('secret');
+        $destination->expects(self::once())->method('getRegion')->willReturn('region');
+        $destination->expects(self::once())->method('getS3Prefix')->willReturn('s3://bucketUrl');
+
+        /** @var Connection|MockObject $conn */
+        $conn = $this->createMock(Connection::class);
+        $conn->expects(self::once())->method('executeQuery')->with(
+            <<<EOT
+COPY INTO 's3://bucketUrl/xxx/path'
+FROM (SELECT * FROM "schema"."table")
+CREDENTIALS = (
+    AWS_KEY_ID = 'key'
+    AWS_SECRET_KEY = 'secret'
+)
+REGION = 'region'
+FILE_FORMAT = (
+    TYPE = 'PARQUET'
+    COMPRESSION='SNAPPY'
+)
+ENCRYPTION = (
+    TYPE = 'AWS_SSE_S3'
+)
+OVERWRITE = TRUE
+MAX_FILE_SIZE = 200000000
+DETAILED_OUTPUT = TRUE
+EOT
+            ,
+            [],
+        );
+
+        $conn->expects(self::once())->method('fetchAllAssociative')
+            ->with('select * from table(result_scan(last_query_id()));')
+            ->willReturn($expectedCopyResult);
+
+        $source = new Storage\Snowflake\Table('schema', 'table');
+        $options = new ExportOptions(true, ExportOptions::MANIFEST_SKIP);
+        $adapter = new S3ParquetExportAdapter($conn);
+
+        $this->assertSame(
+            $expectedCopyResult,
+            $adapter->runCopyCommand(
+                $source,
+                $destination,
+                $options,
+            ),
+        );
+    }
+
+    public function testGetCopyCommandQuery(): void
+    {
+        $expectedCopyResult = [
+            [
+                'FILE_NAME' => 'containerUrl',
+                'FILE_SIZE' => '0',
+                'ROW_COUNT' => '123',
+            ],
+        ];
+
+        /** @var Storage\S3\DestinationFile|MockObject $destination */
+        $destination = $this->createMock(Storage\S3\DestinationFile::class);
+        $destination->expects(self::once())->method('getFilePath')->willReturn('xxx/path');
+        $destination->expects(self::once())->method('getKey')->willReturn('key');
+        $destination->expects(self::once())->method('getSecret')->willReturn('secret');
+        $destination->expects(self::once())->method('getRegion')->willReturn('region');
+        $destination->expects(self::once())->method('getS3Prefix')->willReturn('s3://bucketUrl');
+
+        /** @var Connection|MockObject $conn */
+        $conn = $this->createMock(Connection::class);
+        $conn->expects(self::once())->method('executeQuery')->with(
+            <<<EOT
+COPY INTO 's3://bucketUrl/xxx/path'
+FROM (SELECT * FROM "schema"."table")
+CREDENTIALS = (
+    AWS_KEY_ID = 'key'
+    AWS_SECRET_KEY = 'secret'
+)
+REGION = 'region'
+FILE_FORMAT = (
+    TYPE = 'PARQUET'
+    COMPRESSION='NONE'
+)
+ENCRYPTION = (
+    TYPE = 'AWS_SSE_S3'
+)
+OVERWRITE = TRUE
+MAX_FILE_SIZE = 200000000
+DETAILED_OUTPUT = TRUE
+EOT
+            ,
+            [],
+        );
+
+        $conn->expects(self::once())->method('fetchAllAssociative')
+            ->with('select * from table(result_scan(last_query_id()));')
+            ->willReturn($expectedCopyResult);
+
+        $source = new Storage\Snowflake\SelectSource('SELECT * FROM "schema"."table"');
+        $options = new ExportOptions(false, ExportOptions::MANIFEST_SKIP);
+        $adapter = new S3ParquetExportAdapter($conn);
+
+        $this->assertSame(
+            $expectedCopyResult,
+            $adapter->runCopyCommand(
+                $source,
+                $destination,
+                $options,
+            ),
+        );
+    }
+} 
