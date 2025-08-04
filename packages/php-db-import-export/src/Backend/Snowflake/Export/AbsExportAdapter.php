b02eed02575c9f5a79d902250987c00da7086641
zajca <zajca@zajca.cz>
2025-08-04 16:12:22 +0200

    remove unused condition

--- packages/php-db-import-export/src/Backend/Snowflake/Export/AbsExportAdapter.php
+++ packages/php-db-import-export/src/Backend/Snowflake/Export/AbsExportAdapter.php
@@ -40,10 +40,6 @@ class AbsExportAdapter implements BackendExportAdapterInterface
         Storage\DestinationInterface $destination,
         ExportOptionsInterface $exportOptions,
     ): array {
-        $timestampFormat = 'YYYY-MM-DD HH24:MI:SS';
-        if (in_array(Exporter::FEATURE_FRACTIONAL_SECONDS, $exportOptions->features(), true)) {
-            $timestampFormat = 'YYYY-MM-DD HH24:MI:SS.FF9';
-        }
         $sql = sprintf(
             'COPY INTO \'%s%s\' 
 FROM (%s)
