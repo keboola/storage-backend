b609e3418fab7d33558d4dd2359a699ca83b921c
zajca <zajca@zajca.cz>
2025-09-15 20:26:31 +0200

    register missing handlers

--- packages/php-storage-driver-snowflake/src/Handler/HandlerFactory.php
+++ packages/php-storage-driver-snowflake/src/Handler/HandlerFactory.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Keboola\StorageDriver\Snowflake\Handler;
 
 use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Bucket\CreateBucketCommand;
 use Keboola\StorageDriver\Command\Project\CreateDevBranchCommand;
+use Keboola\StorageDriver\Command\Project\DropDevBranchCommand;
 use Keboola\StorageDriver\Command\Table\CreateProfileTableCommand;
 use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
 use Keboola\StorageDriver\Shared\Driver\Exception\CommandNotSupportedException;
+use Keboola\StorageDriver\Snowflake\Handler\Bucket\CreateBucketHandler;
 use Keboola\StorageDriver\Snowflake\Handler\Project\CreateDevBranchHandler;
+use Keboola\StorageDriver\Snowflake\Handler\Project\DropDevBranchHandler;
 use Keboola\StorageDriver\Snowflake\Handler\Table\ProfileTableHandler;
 
 final class HandlerFactory
@@ -18,7 +22,9 @@ final class HandlerFactory
     {
         return match ($command::class) {
             CreateProfileTableCommand::class => new ProfileTableHandler(),
+            CreateBucketCommand::class => new CreateBucketHandler(),
             CreateDevBranchCommand::class => new CreateDevBranchHandler(),
+            DropDevBranchCommand::class => new DropDevBranchHandler(),
             default => throw new CommandNotSupportedException($command::class),
         };
     }
