e4e47b29e8d1c23335430c057182fb3ddb84c823
Martin Šifra <me@martinsifra.cz>
2025-09-19 15:26:55 +0200

    feat(snowflake driver) Pass logger to SnowflakeDriverClient
    - DMD-357
    - Pass PSR-3 LoggerInterface service to SnowflakeDriverClient.

--- packages/php-storage-driver-snowflake/src/SnowflakeDriverClient.php
+++ packages/php-storage-driver-snowflake/src/SnowflakeDriverClient.php
@@ -9,9 +9,23 @@ use Google\Protobuf\Internal\Message;
 use Keboola\StorageDriver\Command\Common\DriverResponse;
 use Keboola\StorageDriver\Contract\Driver\ClientInterface;
 use Keboola\StorageDriver\Snowflake\Handler\HandlerFactory;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 final class SnowflakeDriverClient implements ClientInterface
 {
+    protected LoggerInterface $logger;
+
+    public function __construct(
+        LoggerInterface|null $logger = null,
+    ) {
+        if ($logger === null) {
+            $this->logger = new NullLogger();
+        } else {
+            $this->logger = $logger;
+        }
+    }
+
     /**
      * @param string[] $features
      */
@@ -29,6 +43,8 @@ final class SnowflakeDriverClient implements ClientInterface
             $features,
             $runtimeOptions,
         );
+
+        $handler->setLogger($this->logger);
 
         $response = new DriverResponse();
         if ($handledResponse !== null) {
