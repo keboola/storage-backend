35c9a980f57920305333da0e24d02ac9f218bc88
zajca <zajca@zajca.cz>
2025-12-01 10:31:40 +0100

    use show tables instead of informations schema

--- packages/php-table-backend-utils/src/TableNotExistsReflectionException.php
+++ packages/php-table-backend-utils/src/TableNotExistsReflectionException.php
@@ -4,22 +4,27 @@ declare(strict_types=1);
 
 namespace Keboola\TableBackendUtils;
 
+use Throwable;
+
 class TableNotExistsReflectionException extends ReflectionException
 {
     /**
      * @param string[] $path
      */
-    public static function createForTable(array $path): self
+    public static function createForTable(array $path, ?Throwable $previous = null): self
     {
-        return new self(sprintf(
-            'Table %s does not exists.',
-            implode(
-                ',',
-                array_map(
-                    fn(string $item) => sprintf('"%s"', $item),
-                    $path,
+        return new self(
+            message: sprintf(
+                'Table %s does not exists.',
+                implode(
+                    ',',
+                    array_map(
+                        fn(string $item) => sprintf('"%s"', $item),
+                        $path,
+                    ),
                 ),
             ),
-        ));
+            previous: $previous,
+        );
     }
 }
